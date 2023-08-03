17adf3b1b86bfde132e8427eab318687e2a6db3b
Diego Ueda <dyhueda@gmail.com>
2023-08-03 21:09:22 +0900

    adding caption

--- src/app/page.js
+++ src/app/page.js
@@ -11,15 +11,16 @@ async function Home() {
       <div className="grid grid-cols-1 gap-4 m-10 md:m-30 md:gap-10 md:grid-cols-2 lg:grid-cols-3">
         {images.map((image) => (
           <div
-            className="bg-slate-300 p-2 rounded-xl h-full w-full hover:shadow-lg"
+            className="bg-slate-300 p-2 rounded-xl hover:shadow-lg"
             key={image.id}
           >
             <a className="" href={image.permalink}>
               <img
-                className=" rounded-xl h-full w-full object-cover"
+                className=" rounded-xl aspect-[4/5] object-cover"
                 src={image.media_url}
                 alt={image.caption}
               />
+              <div>{image.caption}</div>
             </a>
           </div>
         ))}
